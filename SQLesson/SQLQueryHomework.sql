5e214fe8b8b0566151e81493ab24e9f6f02b4c01
iremcaliskan <iremcaliskan@eskisehir.edu.tr>
2021-02-14 17:42:35 +0300

    Sql homework is done.

---
+++ SQLesson/SQLQueryHomework.sql
@@ -0,0 +1,8 @@
+﻿-- Find out how much money we have made in total from each product.
+select p.ProductName as [Product Name], sum((od.UnitPrice*od.Quantity)-((od.UnitPrice*od.Quantity)*od.Discount)) as [Net Winnings]
+from Products p 
+inner join [Order Details] od
+on p.ProductID = od.ProductID
+inner join Orders o
+on o.OrderID = od.OrderID
+group by p.ProductName
